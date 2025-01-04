db5f74f7d1e9fe164eb8853b3aa3c3f7c0d7edd2
Bharath1234511 <bharathmanchana@gmail.com>
2025-01-04 19:53:26 +0530

    Added cors

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ import userRoutes from './routes/userRoutes.js';
 import authMiddleware from './middleware/authMiddleware.js';  
 import dotenv from 'dotenv';  
 import dishRoutes from './routes/dishRoutes.js';
-import transcation from "./routes/trascationroutes.js"
+import transcation from "./routes/trascationroutes.js";
+import cors from 'cors';
 
 const app = express();
 dotenv.config();  
 connectDB();
-app.use(express.json());
 
-app.use(express.json());  
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+ 
 app.use('/api/ingredients', ingredientRoutes); 
 app.use('/api/users', userRoutes);  
 app.use('/api/dishes', dishRoutes);
